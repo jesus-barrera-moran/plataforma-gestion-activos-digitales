8697536e28c37f31b20f6bca9d26304d49a5166a
jesus-barrera-moran <jesus.barrera.moran@gmail.com>
2024-10-18 16:58:45 -0600

    Deshabilitar boton

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -3,11 +3,12 @@ import React from "react";
 //INTERNAL IMPORT
 import Style from "./Button.module.css";
 
-const Button = ({ btnName, handleClick, icon, classStyle }) => {
+const Button = ({ btnName, handleClick, icon, classStyle, disabled = false }) => {
   return (
     <div className={Style.box}>
       <button
-        className={`${Style.button} ${classStyle}`}
+        disabled={disabled}
+        className={`${disabled ? Style.button_disabled : Style.button} ${classStyle}`}
         onClick={() => handleClick()}
       >
         {icon} {btnName}
